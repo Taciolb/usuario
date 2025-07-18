8b8afb603cc04591ee611d3bac3a28122cb7bea2
Taciano <taciolb@gmail.com>
2025-07-17 22:18:53 -0300

    adicionado put de endereço e telefone

--- src/main/java/com/tlbtech/usuario/controller/UsuarioController.java
+++ src/main/java/com/tlbtech/usuario/controller/UsuarioController.java
@@ -1,8 +1,9 @@
 package com.tlbtech.usuario.controller;
 
 import com.tlbtech.usuario.business.UsuarioService;
+import com.tlbtech.usuario.business.dto.EnderecoDTO;
+import com.tlbtech.usuario.business.dto.TelefoneDTO;
 import com.tlbtech.usuario.business.dto.UsuarioDTO;
-import com.tlbtech.usuario.infrastructure.entity.Usuario;
 import com.tlbtech.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -35,7 +36,7 @@ public class UsuarioController {
     }
 
     @GetMapping
-    public ResponseEntity<Usuario> buscaUsuarioPorEmail(@RequestParam("email") String email){
+    public ResponseEntity<UsuarioDTO> buscaUsuarioPorEmail(@RequestParam("email") String email){
         return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
     }
 
@@ -49,5 +50,17 @@ public class UsuarioController {
     public ResponseEntity<UsuarioDTO> atualizDadoUsuario(@RequestBody UsuarioDTO dto,
                                                          @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token, dto));
+    }
+
+    @PutMapping("/endereco")
+    public ResponseEntity<EnderecoDTO> atualizaEndereco(@RequestBody EnderecoDTO dto,
+                                                        @RequestParam("id") Long id){
+        return ResponseEntity.ok(usuarioService.atualizaEndereço(id, dto));
+    }
+
+    @PutMapping("/telefone")
+    public ResponseEntity<TelefoneDTO> atualizaTelefone(@RequestBody TelefoneDTO dto,
+                                                        @RequestParam("id") Long id){
+        return ResponseEntity.ok(usuarioService.atualizaTelefone(id, dto));
     }
 }
