937620faa07020080fa81cb80624ba04de735c9f
Taciano <taciolb@gmail.com>
2026-02-15 10:54:58 -0300

    adicionados metodos get, delete e login

--- src/main/java/com/tlbtech/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/tlbtech/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.tlbtech.usuario.infrastructure.repository;
 
-import com.taciolb.aprendendospring.infrastructure.entity.Endereco;
+
+import com.tlbtech.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/tlbtech/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/tlbtech/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,7 @@
 package com.tlbtech.usuario.infrastructure.repository;
 
-import com.taciolb.aprendendospring.infrastructure.entity.Telefone;
+
+import com.tlbtech.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
